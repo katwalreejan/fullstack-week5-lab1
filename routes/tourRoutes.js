b4abe5e7fa028813d42f1baff8e732b1730ae429
unknown <Reejan.Katwal@metropolia.fi>
2025-11-18 10:52:37 +0200

    Refactor: Reorganize tours API into MVC pattern

---
+++ routes/tourRoutes.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const router = express.Router();
+
+const {
+  getAllTours,
+  getTourById,
+  createTour,
+  updateTour,
+  deleteTour,
+} = require("../controllers/tourControllers.js"); 
+
+// Middleware to parse JSON
+
+// ROUTES
+// GET /tours
+router.get("/tours", getAllTours);
+
+// POST /tours
+router.post("/tours", createTour);
+
+// GET /tours/:tourId
+router.get("/tours/:tourId", getTourById);
+
+// PUT /tours/:tourId
+router.put("/tours/:tourId", updateTour);
+
+// DELETE /tours/:tourId
+router.delete("/tours/:tourId", deleteTour);
+
+module.exports=router
